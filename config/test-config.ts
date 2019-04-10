aec9f595ce43df29775cce86e43574c42c2a25f4
Michalk39 <michalk9339@gmail.com>
2019-04-10 09:48:01 +0200

    baseUrl changed for ci

--- config/test-config.ts
+++ config/test-config.ts
@@ -1,6 +1,6 @@
 export const testConfig = {
-    baseUrl: process.env.baseUrl || "http://local.magento/",
+    baseUrl: process.env.baseUrl || "http://127.0.0.1/magento/",
     adminLogin: "admin",
-    adminPassword: "magentorocks1",
+    adminPassword: "123123q",
     adminWrongPassword: "xxx",
 };
