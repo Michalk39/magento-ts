ccd50fe08baaf44dcef33753043eadd4ca0ed1b0
Michalk39 <michalk9339@gmail.com>
2019-04-11 15:44:37 +0200

    Fixes for jenkins

--- pages/app/magentoCustomerGroupsEdit.ts
+++ pages/app/magentoCustomerGroupsEdit.ts
@@ -2,6 +2,7 @@ import { $, ElementFinder, ActionSequence } from "protractor";
 import { Actions } from "../../support/actions";
 import { BrowserActions } from "../../support/browser";
 import { CustomWait } from "../../support/wait";
+import { stringify } from "querystring";
 
 export class MagentoCustomerGroupsEdit {
     private groupNameField: ElementFinder;
@@ -17,8 +18,15 @@ export class MagentoCustomerGroupsEdit {
                 " \nnameFromField: " +
                 (await this.groupNameField.getAttribute("value"))
         );
+
+        if (name == (await this.groupNameField.getAttribute("value"))) {
+            console.log("PODOBNE");
+        }
+        if (name === (await this.groupNameField.getAttribute("value"))) {
+            console.log("IDENTYCZNE");
+        }
         await CustomWait.waitForElementToBeClickable(this.groupNameField);
-        return await this.groupNameField.getAttribute("value");
+        return await stringify(this.groupNameField.getAttribute("value"));
     }
 
     public async isGroupNameFieldDisable() {
